a97e39411a085b76d0011a8b90abdae63f3d7e3b
Angellsworth <angellsworth@msn.com>
2025-04-02 17:46:14 -0600

    wip: added userSchema with quiz results

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,3 +1,4 @@
+// models/user.js - w/quiz results
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
@@ -5,15 +6,41 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+  },
   hashedPassword: {
     type: String,
     required: true,
   },
+  profileImg: {
+    type: String,
+    default: '',
+  },
+  pinnedOutfits: [{
+    type: mongoose.Schema.Types.ObjectId,// use .populate() to get outfit details
+    ref: 'Outfit',
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  //embedded quiz results
+  genderCategory: String,
+  styleProfile: String,
+  lifestyleTags: [String],
+  climateRegion: String,
+  seasonalPrefs: [String],
+  fitPreference: String,
+  quizTakenAt: Date
+
 });
 
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     delete returnedObject.hashedPassword;
+    return returnedObject
   }
 });
 
